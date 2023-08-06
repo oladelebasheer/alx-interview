8eec7f0d81cd9ea9c0d29b450bf78fd5ce072d6e
Oladele Basheer <43867041+oladelebasheer@users.noreply.github.com>
2023-08-06 22:13:40 +0100

    new update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        nn = int(n[1])
+        n = int(n[1])
         if nn < 4:
             print("N must be at least 4")
             sys.exit(1)
